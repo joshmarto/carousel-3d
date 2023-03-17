7b21e15e5d2289f00b01e71cce5d745fa070643f
Josue Martinez <jmartinezolivares1@gmail.com>
2023-03-17 14:15:44 -0600

    Restyle page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const elementosCarousel = document.querySelectorAll('.carousel');
-  M.Carousel.init(elementosCarousel, {
+  const car = M.Carousel.init(elementosCarousel, {
     duration: 150,
     dist: -80,
     shift: 5,
     padding: 5,
-    numVisible: 3,
+    numVisible: 5,
     indicators: true,
     noWrap: false
   });
+
+  const interval = setInterval(() => {
+    var instance = M.Carousel.getInstance(elem);
+    instance.next(car);
+  }, 3000);
+  interval();
 });
